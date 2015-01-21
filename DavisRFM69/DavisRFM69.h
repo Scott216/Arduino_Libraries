be01caf54dc760543f9b82857af4365420156701
Scott Goldthwaite <scott@goldthwaite.com>
2015-01-21 05:54:53 -0500

    Added Compatibility for MoteinoMega
    Added #ifdef that changes interrupts and SS pin for different MCU.
    Lowered SPI speed from DIV2 to DIV4
    Moved SPI calls from initialize() to select()
    Made sure SS pin was set as output

--- DavisRFM69/DavisRFM69.h
+++ DavisRFM69/DavisRFM69.h
@@ -23,12 +23,26 @@
 //#define DAVIS_FREQS_AU
 //#define DAVIS_FREQS_NZ
 
-#include <Davisdef.h>
-#include <Arduino.h>            //assumes Arduino IDE v1.0 or greater
+#include <Davisdef.h>           // http://github.com/dekay/DavisRFM69/blob/master/Davisdef.h
+#include <Arduino.h>            // Assumes Arduino IDE v1.0 or greater
 
 #define DAVIS_PACKET_LEN      8 // ISS has fixed packet lengths of eight bytes, including CRC
 #define SPI_CS               SS // SS is the SPI slave select pin, for instance D10 on atmega328
-#define RF69_IRQ_PIN          2 // INT0 on AVRs should be connected to DIO0 (ex on Atmega328 it's D2)
+
+// SRG  Added if defined  for MoteinoMega comp ability
+//      Ref: https://github.com/LowPowerLab/RFM69/commit/e8a076784954da19acd2009b12e23f15e5894b8d
+// INT0 on AVRs should be connected to RFM69's DIO0 (ex on ATmega328 it's D2, on ATmega644/1284 it's D2)
+#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega88) || defined(__AVR_ATmega8__) || defined(__AVR_ATmega88__)
+  #define RF69_IRQ_PIN          2
+  #define RF69_IRQ_NUM          0
+#elif defined(__AVR_ATmega644P__) || defined(__AVR_ATmega1284P__)
+  #define RF69_IRQ_PIN          2
+  #define RF69_IRQ_NUM          2
+#elif defined(__AVR_ATmega32U4__)
+  #define RF69_IRQ_PIN          3
+  #define RF69_IRQ_NUM          0
+#endif
+
 #define CSMA_LIMIT          -90 // upper RX signal sensitivity threshold in dBm for carrier sense access
 #define RF69_MODE_SLEEP       0 // XTAL OFF
 #define RF69_MODE_STANDBY     1 // XTAL ON
@@ -47,9 +61,13 @@ class DavisRFM69 {
     static volatile byte CHANNEL;
     static volatile int RSSI;
 
-    DavisRFM69(byte slaveSelectPin=SPI_CS, byte interruptPin=RF69_IRQ_PIN, bool isRFM69HW=false) {
+
+  //  SRG See: http://github.com/LowPowerLab/RFM69/blob/e8a076784954da19acd2009b12e23f15e5894b8d/RFM69.h#L78
+
+    DavisRFM69(byte slaveSelectPin=SPI_CS, byte interruptPin=RF69_IRQ_PIN, bool isRFM69HW=false, byte interruptNum=RF69_IRQ_NUM) {
       _slaveSelectPin = slaveSelectPin;
       _interruptPin = interruptPin;
+      _interruptNum = interruptNum;
       _mode = RF69_MODE_STANDBY;
       _packetReceived = false;
       _powerLevel = 31;
@@ -62,7 +80,7 @@ class DavisRFM69 {
     void hop();
     unsigned int crc16_ccitt(volatile byte *buf, byte len, unsigned int initCrc = 0);
 
-    void initialize();
+    bool initialize();
     bool canSend();
     void send(const void* buffer, byte bufferSize);
     bool receiveDone();
@@ -90,6 +108,7 @@ class DavisRFM69 {
     static DavisRFM69* selfPointer;
     byte _slaveSelectPin;
     byte _interruptPin;
+    byte _interruptNum;
     byte _powerLevel;
     bool _isRFM69HW;
 
